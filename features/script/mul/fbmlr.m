fec0ccd19282af33b8ff0e35609337e1ee8176d1
Yao <liuyaoly@umich.edu>
2014-12-18 21:27:24 -0500

    clean

--- features/script/mul/fbmlr.m
+++ features/script/mul/fbmlr.m
@@ -2,7 +2,7 @@ load('../../../data/matlab/one_hot_data.mat');
 % 
 % %%%%%%Can only have positive tags
 y_train = ((y_train==1) + 1);
-[out] =  fsSBMLR(x_train, y_train);
+% [out] =  fsSBMLR(x_train, y_train);
 fileID = fopen('../../features_data/mul/onehot/fsSBMLR_features_idx.txt','w');
 fprintf(fileID,'Algorithm:fsSBMLR\n');
 fprintf(fileID,'DataFile:one_hot_data.mat\n');
